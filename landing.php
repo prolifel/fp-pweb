1e0392cf56c89f7f83efd14b40fe6a5cecf58f9a
NaufalRafi-hub <rafi.harahaps@gmail.com>
2021-01-09 22:52:13 +0700

    guru udah cantik

--- landing.php
+++ landing.php
@@ -128,6 +128,10 @@
         
     </style>
     <link rel="stylesheet" href="./css/bootstrap.min.css">
+    <!-- <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"> -->
 </head>
 <body>
     <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-success">
@@ -157,9 +161,11 @@
             </form>
         </div>
     </nav>
-
-    
+    <!--  -->
+    <!-- <div class="container-fluid"> -->
         <div class="d-flex justify-content-center">
+        
+            
             <?php
                 if(isset($_GET)){
                     if($_GET['msg']=="fail"){
@@ -168,8 +174,9 @@
                 }
                 include './admin/formLogin.php';
             ?>
+            
         </div>
-        
+ 
     
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script>window.jQuery || document.write('<script src="../assets/js/vendor/jquery.slim.min.js"><\/script>')</script>

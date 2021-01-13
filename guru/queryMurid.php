69c74b650242bacc5345f30130a9392bbad0dde8
EdoYogatama <roronoadezoro@gmail.com>
2021-01-13 07:25:09 -0500

    Bisa input nilai siswa/update nilai siswa

--- guru/queryMurid.php
+++ guru/queryMurid.php
@@ -3,7 +3,7 @@
         if(isset($_SESSION['id'])){
             $idGuru = $_SESSION['id'];
             include "../koneksi.php";
-            $string = "select users.id, users.nama_user from users,kelas_siswa where users.id=kelas_siswa.siswa_id and kelas_siswa.guru_id=$idGuru";
+            $string = "select users.id, users.nama_user from users,kelas_siswa where users.id=kelas_siswa.siswa_id and kelas_siswa.guru_id=$idGuru order by users.nama_user";
             $query = mysqli_query($koneksi, $string);
             if(mysqli_num_rows($query)>0){
                 return $query;
--- guru/uploadNilai.php
+++ guru/uploadNilai.php
@@ -2,6 +2,44 @@
     include "../koneksi.php";
     $idmp = null;
     if(isset($_GET['id']) &&  isset($_GET['idg']) && isset($_GET['mp'])){
-        switch()
+        $mp = $_GET['mp'];
+        $idSiswa = $_GET['id'];
+        $idGuru = $_GET['idg'];
+        switch($_GET['mp']){
+            case "biologi":
+                $idmp = 1;
+                break;
+            case "fisika":
+                $idmp = 2;
+                break;
+            case "kimia":
+                $idmp = 3;
+                break;
+            case "math":
+                $idmp = 4;
+                break;
+        }
+    }
+
+    $tugas1 = $_POST['tugas1'];
+    $tugas2 = $_POST['tugas2'];
+    $uts    = $_POST['uts'];
+    $uas    = $_POST['uas'];
+    $prak   = $_POST['prak'];
+
+    if(mysqli_query($koneksi, "select id_nilai_siswa from nilai_siswa where id_mapel=$idmp and id_guru=$idGuru and id_siswa=$idSiswa")){
+        $qstring = "update nilai_siswa set tugas1=$tugas1,tugas2=$tugas2,uts=$uts,uas=$uas,praktikum=$prak where id_guru=$idGuru and id_siswa=$idSiswa and id_mapel=$idmp";
+        $queryres = mysqli_query($koneksi, $qstring);
+        if($queryres) {
+            header("Location: nilai.php?id=$mp");
+            exit();
+        }
+    }
+    $qstring = "insert into nilai_siswa(id_nilai_siswa, id_mapel, id_guru, id_siswa, tugas1, tugas2, uts, uas, praktikum, komentar) 
+                values (null, $idmp, $idGuru, $idSiswa, $tugas1, $tugas2, $uts, $uas, $prak, null)";
+    $queryres = mysqli_query($koneksi, $qstring);
+    if($queryres) {
+        header("Location: nilai.php?id=$mp");
+        exit();
     }
 ?>
